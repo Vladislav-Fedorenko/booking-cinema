e6821ea4a6f43b1296af17e95adf294a376a4295
Vladislav Fedorenko <fedorenko.vladislav.k@gmail.com>
2021-05-22 14:56:22 +0600

    [+] Project has been initialized

---
+++ src/main/kotlin/tld/sld/bookingcinemaseats/BookingCinemaSeatsApplication.kt
@@ -0,0 +1,11 @@
+package tld.sld.bookingcinemaseats
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookingCinemaSeatsApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookingCinemaSeatsApplication>(*args)
+}
---
+++ src/test/kotlin/tld/sld/bookingcinemaseats/BookingCinemaSeatsApplicationTests.kt
@@ -0,0 +1,13 @@
+package tld.sld.bookingcinemaseats
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookingCinemaSeatsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
